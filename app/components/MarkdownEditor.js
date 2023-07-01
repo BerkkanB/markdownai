6f7c144ef25424573160d7ca2b603898a4953c8d
BerkkanB <butun18@itu.edu.tr>
2023-07-02 01:04:13 +0300

    CodeMirror dependencies implemented

---
+++ app/components/MarkdownEditor.js
@@ -0,0 +1,37 @@
+"use client";
+
+import CodeMirror from '@uiw/react-codemirror';
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { languages } from '@codemirror/language-data';
+import { tokyoNightStorm } from '@uiw/codemirror-themes-all';
+
+const code = `## Title
+
+\`\`\`jsx
+function Demo() {
+  return <div>demo</div>
+}
+\`\`\`
+
+\`\`\`bash
+# Not dependent on uiw.
+npm install @codemirror/lang-markdown --save
+npm install @codemirror/language-data --save
+\`\`\`
+
+[weisit ulr](https://uiwjs.github.io/react-codemirror/)
+
+\`\`\`go
+package main
+import "fmt"
+func main() {
+  fmt.Println("Hello, 世界")
+}
+\`\`\`
+`;
+
+export default function MarkdownEditor() {
+    return (
+        <CodeMirror className="h-screen" theme={tokyoNightStorm} value={code} extensions={[markdown({ base: markdownLanguage, codeLanguages: languages })]} />
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
+import MarkdownEditor from "./components/MarkdownEditor";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-10 bg-gray-100">
-    <div className="max-w-2xl w-full px-6 py-8 bg-white rounded-lg shadow-lg">
-      <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
-        Markdown Editor
-      </h1>
+    <div className="flex h-screen">
+      <div className="w-1/2 h-full bg-gray-100">
+        <MarkdownEditor />
+      </div>
+      <div className="w-1/2 h-full bg-gray-200">asd</div>
     </div>
-  </div>
+
   )
 }
