004c1d7e2eeeacf541b36178e2c54243fb1158be
BerkkanB <butun18@itu.edu.tr>
2023-07-03 11:30:31 +0300

    code mirror removed

--- app/page.js
+++ app/page.js
@@ -1,10 +1,7 @@
-import MarkdownEditor from "./components/MarkdownEditor";
-
 export default function Home() {
   return (
     <div className="flex h-screen">
       <div className="w-1/2 h-full bg-gray-100">
-        <MarkdownEditor />
       </div>
       <div className="w-1/2 h-full bg-gray-200">asd</div>
     </div>
