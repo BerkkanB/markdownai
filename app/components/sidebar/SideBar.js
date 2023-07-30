17de836c237ab4d6c37da763872e28f1c60bbbf1
BerkkanB <butun18@itu.edu.tr>
2023-07-30 16:28:17 +0300

    navbar and sidebar ui added

---
+++ app/components/sidebar/SideBar.js
@@ -0,0 +1,12 @@
+import Navbar from "../navbar/Navbar";
+import SideNavBar from "./SideNavBar";
+
+const SideBar = () => {
+    return (
+        <div className="sidebar">
+            <SideNavBar />
+        </div>
+    );
+}
+
+export default SideBar;
--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Tiptap from "./components/EditorContent";
+import Navbar from "./components/navbar/Navbar";
+import SideBar from "./components/sidebar/SideBar";
 
 export default function Home() {
   return (
-    <div className="flex h-screen bg-white">
-      <div className="h-full w-full">
+    <div className="flex h-screen bg-white flex-col ">
+      <Navbar />
+      <div className="flex flex-row"> 
+        <SideBar />
         <Tiptap />
       </div>
     </div>
