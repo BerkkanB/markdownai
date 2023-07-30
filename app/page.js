8f89d4cdeeb2aecb1bfea2d7770e0f09e620d432
BerkkanB <butun18@itu.edu.tr>
2023-07-30 15:41:21 +0300

    ui updates

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Tiptap from "./components/EditorContent";
 
 export default function Home() {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-white">
       <div className="h-full w-full">
         <Tiptap />
       </div>
